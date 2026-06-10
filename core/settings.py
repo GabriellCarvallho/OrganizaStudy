6937f452f3760339c0d2bc0bfd469ffcfbcf1665
Gabriel Pereira de Carvalho <137663049+GabriellCarvallho@users.noreply.github.com>
2026-06-10 11:01:51 -0300

    feat: autenticação JWT com simplejwt

--- core/settings.py
+++ core/settings.py
@@ -105,6 +105,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
