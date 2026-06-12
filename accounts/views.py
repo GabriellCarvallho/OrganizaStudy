8e1c5a2b729defb054d0a2d8833451fce28486ac
Gabriel Pereira de Carvalho <137663049+GabriellCarvallho@users.noreply.github.com>
2026-06-12 13:03:51 -0300

    feat: tela dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import get_user_model
-
+from django.contrib.auth.decorators import login_required
 
 User = get_user_model()
 
@@ -35,4 +35,10 @@ def register_view(request):
         return redirect('/dashboard/')
 
     return render(request, 'accounts/register.html')
+
+
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'dashboard/index.html')
     
