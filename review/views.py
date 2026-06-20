06408492afddf3a0b6369e575826b7fb14efc982
Gabriel Pereira de Carvalho <gabrielwastowski2@gmail.com>
2026-06-20 14:00:43 -0300

    feat: integração jwt completa

--- review/views.py
+++ review/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -14,12 +11,8 @@ class DeckViewSet(viewsets.ModelViewSet):
     serializer_class = DeckSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-
-
     def get_queryset(self):
         return Deck.objects.filter(user=self.request.user)
-    
-
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -29,27 +22,25 @@ class FlashcardViewSet(viewsets.ModelViewSet):
     serializer_class = FlashcardSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-
-
     def get_queryset(self):
         return Flashcard.objects.filter(deck__user=self.request.user)
-    
+
     @action(detail=True, methods=['post'])
-    def review(self, requst, pk=None):
+    def review(self, request, pk=None):
         flashcard = self.get_object()
         quality = request.data.get('quality')
 
-        if quality is None or not (0 <= quality <= 5):
+        if quality is None or not (0 <= int(quality) <= 5):
             return Response({'error': 'quality deve estar entre 0 e 5'}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         ease_factor, interval = calculate_sm2(flashcard.ease_factor, flashcard.interval, int(quality))
         flashcard.ease_factor = ease_factor
         flashcard.interval = interval
-        flash.card.next_review = timezone.now() + timedelta(days=interval)
+        flashcard.next_review_date = timezone.now() + timedelta(days=interval)
         flashcard.save()
 
         return Response({
             'ease_factor': ease_factor,
             'interval': interval,
-            'next_review': flashcard.next_review
+            'next_review_date': flashcard.next_review_date
         })
